cbca8ad717f719b385a4182961ef8fff808d6fa4
Thomas Morales <moraless@MoralesPC.localdomain>
2025-11-11 17:35:14 +0100

    feat: modifica del file test

--- test/test_calcoli.py
+++ test/test_calcoli.py
@@ -7,7 +7,7 @@ from src_exercises.calcoli import somma, moltiplica
 def test_somma():
     assert somma(2, 3) == 5
     assert somma(-1, 1) == 0
-    assert somma(0, 0) == 0
+    assert somma(0, 0) == 0 
 
 def test_moltiplica():
     assert moltiplica(2, 3) == 6
